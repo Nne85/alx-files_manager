a01e8f6db25b080b9a9a8ebe5d5bab1e4a35dd12
Nnenna Kehinde <nneukamaka@yahoo.com>
2024-06-12 05:48:55 -0700

    Eslint fixed

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
 import express from 'express';
-import AuthController from '../controllers/AuthController.js';
+//import AuthController from '../controllers/AuthController';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
+//import UsersController from '../controllers/UsersController';
+//import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 const routeController = (app) => {
   app.use('/', router);
-	
-  router.get('/status' , (req, res) => {
+
+  router.get('/status', (req, res) => {
     AppController.getStatus(req, res);
-});
+  });
 
   router.get('/stats', (req, res) => {
     AppController.getStats(req, res);
-});
+  });
 
-  router.get('/users/me', (req, res) => {
+  /*router.get('/users/me', (req, res) => {
     UsersController.getMe(req, res);
   });
 
@@ -31,6 +32,29 @@ const routeController = (app) => {
     AuthController.getDisconnect(req, res);
   });
 
-}
+  router.post('/files', (req, res) => {
+    FilesController.postUpload(req, res);
+  });
+
+  router.get('/files/:id', (req, res) => {
+    FilesController.getShow(req, res);
+  });
+
+  router.get('/files', (req, res) => {
+    FilesController.getIndex(req, res);
+  });
+
+  router.put('/files/:id/publish', (req, res) => {
+    FilesController.putPublish(req, res);
+  });
+
+  router.put('/files/:id/unpublish', (req, res) => {
+    FilesController.putUnpublish(req, res);
+  });
+
+  router.post('/files/:id/data', (req, res) => {
+    FilesController.getFile(req, res);
+  });*/
+};
 
 export default routeController;
